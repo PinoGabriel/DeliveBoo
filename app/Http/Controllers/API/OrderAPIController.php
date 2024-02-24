48b684dd615ee8e84a029a86aae25ab54d7963f3
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-24 17:03:13 +0100

    Implement Order api controller

---
+++ app/Http/Controllers/API/OrderAPIController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'success' => true,
+            'data' => Order::with(['restaurant', 'dishes'])->get()
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json([
+            'success' => true,
+            'data' => Order::with(['restaurant', 'dishes'])->find($id)
+        ]);
+    }
+}
