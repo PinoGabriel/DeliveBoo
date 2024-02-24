1cf838b688116e8d204040ea16eceea77c7a476c
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-24 16:33:39 +0100

    Add Type fillable propriety

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
-    
+    protected $fillable = [
+        'name',
+        'img',
+        'description'
+    ];
+
     public function restaurants()
     {
         return $this->belongsToMany(Restaurant::class);
