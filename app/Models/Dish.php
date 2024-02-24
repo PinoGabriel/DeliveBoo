47252ab0c0fad3e26c5d25b998c99f6574bfbff9
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-24 18:14:13 +0100

    Add quantity attribute

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -23,6 +23,6 @@ class Dish extends Model
 
     public function orders()
     {
-        return $this->belongsToMany(Order::class);
+        return $this->belongsToMany(Order::class)->withPivot('quantity');
     }
 }
