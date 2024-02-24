a6766f226f041bcddd41702c3950e105d6958d3a
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-24 17:03:03 +0100

    Implement Dish api controller

---
+++ app/Http/Controllers/API/DishAPIController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Dish;
+
+class DishAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'success' => true,
+            'data' => Dish::with(['restaurant', 'orders'])->get()
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json([
+            'success' => true,
+            'data' => Dish::with(['restaurant', 'orders'])->find($id)
+        ]);
+    }
+}
