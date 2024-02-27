f70f663d43a78c34b51a671344aea9bab7133a9f
Zazzaro <sassaro01cristian@gmail.com>
2024-02-27 17:46:08 +0100

    completed restaurants img and started dish img

--- config/restaurant.php
+++ config/restaurant.php
@@ -3,70 +3,70 @@
 return [
     [
         'name' => 'Gusto Divino',
-        'img' => 'placeholder',
+        'img' => 'https://www.cosenzaduepuntozero.it/wp-content/uploads/Gusto-Divino-Rende-cop.jpg',
         'description' => '
         Benvenuti al Gusto Divino! Godetevi autentiche pizze con crosta sottile e piatti italiani tradizionali preparati dal nostro chef esperto. Con atmosfera accogliente e dettagli rustici, offriamo un esperienza culinaria che unisce tradizione e creatività in ogni boccone. Perfetto per serate romantiche o incontri conviviali con amici e familiari.',
         'address' => 'Via delle Sere Luminose, 42, Quartiere Incantato, 00123 Città Sogno, Italia',
     ],
     [
         'name' => 'DragonWok & Sushi Haven',
-        'img' => 'placeholder',
+        'img' => 'https://static.wixstatic.com/media/29ec54_12ca1270abe2486680dde57e72c0ed59~mv2.jpg/v1/fill/w_1000,h_563,al_c,q_85,usm_0.66_1.00_0.01/29ec54_12ca1270abe2486680dde57e72c0ed59~mv2.jpg',
         'description' => '
         Benvenuti al DragonWok & Sushi Haven, dove l arte della cucina cinese si fonde armoniosamente con la freschezza del sushi giapponese. Il nostro ristorante è un rifugio culinario che offre una vasta gamma di delizie orientali, dalle croccanti specialità del wok alle creazioni artistiche del sushi.',
         'address' => 'Via Orientale, 88, Quartiere Zen, 12345 Metropoli Esotica, Italia',
     ],
     [
         'name' => 'Sabor y Asador',
-        'img' => 'placeholder',
+        'img' => 'https://lh5.googleusercontent.com/p/AF1QipNRte3wVcSlYh6wy9pIDKdXxzgIEJ3BOpKtRlvw',
         'description' => '
         Benvenuti a Sabor y Asador, il paradiso del sapore messicano e delle prelibatezze di carne. Situato in Calle del Sazón nel cuore del Barrio del Fuego, il nostro ristorante offre un esplosione di sapori autentici dalla cucina messicana, accompagnata da selezioni di carne alla griglia irresistibili.',
         'address' => 'Via del Sapore, 55, Quartiere del Fuoco, 56789 Città del Sapore, Italia',
     ],
     [
         'name' => 'Senza Glutine Delight',
-        'img' => 'placeholder',
+        'img' => 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/4a/40/0d/osteria-alle-volte.jpg?w=600&h=400&s=1',
         'description' => '
         Benvenuti a Senza Glutine Delight in Via Libera, Quartiere Salute. Il nostro ristorante è dedicato completamente al mondo senza glutine, offrendo piatti deliziosi e creativi senza compromettere il gusto. Un esperienza gastronomica senza preoccupazioni per gli amanti del cibo sano e gustoso.',
         'address' => 'Via Libera, 24, Quartiere Salute, 12345 Città Vitalità, Italia',
     ],
     [
         'name' => 'Verde Gastronomia',
-        'img' => 'placeholder',
+        'img' => 'https://media-cdn.tripadvisor.com/media/photo-s/19/3c/ea/5b/20190913-185902-largejpg.jpg',
         'description' => '
         Verde Gastronomia, nel Quartiere Vegano, propone una cucina vegana e vegetariana con ingredienti freschi e sostenibili. Dai colorati piatti gourmet a quelli caldi e avvolgenti, ogni creazione riflette l impegno per una cucina che celebra gusto e salute, offrendo un esperienza culinaria completa per il benessere di corpo e anima.',
         'address' => 'Via dei Sapori Verdi, 33, Quartiere Vegano, 56789 Città Sana, Italia',
     ],
     [
         'name' => 'Dolci Delizie',
-        'img' => 'placeholder',
+        'img' => 'https://www.dovepasticcio.it/img/bakeries/98/1.jpeg',
         'description' => '
         Benvenuti a Dolci Delizie, il paradiso per gli amanti del dolce. Situato in Via dei Dolci nel Quartiere Gelato, offriamo un ampia selezione di dessert irresistibili e gelati artigianali. Dal gusto classico alle creazioni più audaci, ogni boccone è un viaggio nel mondo dei sapori dolci, creando un atmosfera di puro piacere per i vostri sensi.',
         'address' => 'Via dei Dolci, 12, Quartiere Gelato, 12345 Città Golosa, Italia',
     ],
     [
         'name' => 'QuickBite Burger Hub',
-        'img' => 'placeholder',
+        'img' => 'https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_300,q_100,fl_lossy,dpr_2.0,c_fit,f_auto,h_300/t2yccyupge3t5a3blfcw',
         'description' => '
         QuickBite Burger Hub, situato in Via del Gusto Veloce nel Quartiere FastFood, è il rifugio dell hamburger veloce. Offriamo un ampia selezione di deliziosi hamburger e fast food, garantendo sapori irresistibili e un esperienza di gusto rapida e soddisfacente per chi è sempre in movimento.',
         'address' => 'Via del Gusto Veloce, 55, Quartiere FastFood, 56789 Città Veloce, Italia',
     ],
     [
         'name' => 'Sapore Vegano',
-        'img' => 'placeholder',
+        'img' => 'https://www.zucchinaverde.it/public/locali/foto/social_1441.jpg',
         'description' => '
         Sapore Vegano, in Via degli Aromi nel Quartiere Verde, è un ristorante che celebra la cucina italiana in chiave vegana. Con ingredienti freschi e creatività culinaria, offriamo piatti deliziosi che catturano l autenticità dei sapori italiani senza compromettere il rispetto per gli animali. Un esperienza culinaria che delizia i sensi e nutre l anima',
         'address' => 'Via degli Aromi, 15, Quartiere Verde, 12345 Città Gustosa, Italia',
     ],
     [
         'name' => 'Mare & Delizia',
-        'img' => 'placeholder',
+        'img' => 'https://10619-2.s.cdn12.com/rests/original/102_521391549.jpg',
         'description' => '
         Mare & Delizia, situato in Via dei Pescatori nel Quartiere Sapore di Mare, è il rifugio perfetto per gli amanti del pesce fresco. Offriamo piatti raffinati e deliziosi, celebrando autentici sapori marini con ingredienti di alta qualità. Un esperienza gastronomica che incarna la bontà del mare in ogni boccone.',
         'address' => 'Via dei Pescatori, 20, Quartiere Sapore di Mare, 12345 Città del Pesce, Italia',
     ],
     [
         'name' => 'Kebab Kingdom',
-        'img' => 'placeholder',
+        'img' => 'https://lh3.googleusercontent.com/p/AF1QipPuHsRjf1ERlb2DOvWaApmFyviiTHhmYggHsF95=w1080-h608-p-no-v0',
         'description' => '
         Kebab Kingdom, in Via delle Spezie nel Quartiere Kebab Street, offre un autentica esperienza culinaria kebab. Con ingredienti freschi e sapori speziati, ogni boccone è un viaggio delizioso in un regno di autentiche prelibatezze.',
         'address' => 'Via delle Spezie, 30, Quartiere Kebab Street, 56789 Città Saporita, Italia',
