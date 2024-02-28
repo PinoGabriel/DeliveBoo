11eb2b1a26607d521a7afb16e052a6f6fd397af1
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-28 12:12:47 +0100

    Bugfix: resolve merge conflicts

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -40,92 +40,55 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="mb-3">
-                            <label for="description" class="form-label">Descrizione</label>
-
-                            <input type="text" class="form-control @error('description') is-invalid @enderror"
-                                id="description" name="description" value="{{ old('description') ?? '' }}">
-                            @error('description')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="p_iva" class="form-label">Partita Iva:</label>
-                            <input type="text" class="form-control @error('p_iva') is-invalid @enderror" id="p_iva"
-                                name="p_iva" value = "{{ old('p_iva') ?? '' }}">
-                            @error('p_iva')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="address" class="form-label">Indirizzo</label>
-                            <input type="text" class="form-control @error('address') is-invalid @enderror" id="address"
-                                name="address" value="{{ old('address') ?? '' }}">
+                        <textarea rows="6" class="form-control @error('description') is-invalid @enderror" id="description"
+                            name="description" placeholder="Inserisci una descrizione">{{ old('description') ?? '' }}</textarea>
+                        @error('description')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                        <div class="mb-3 row">
+                            <div class="col-12 col-md-6 mb-3">
+                                <label for="address" class="form-label">Indirizzo*</label>
+                                <input type="text" class="form-control @error('address') is-invalid @enderror" id="address"
+                                    name="address" value="{{ old('address') ?? '' }}"
+                                    placeholder="Via, civico, città, provincia, CAP" required>
+                                @error('address')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-12 col-md-6 mb-3">
+                                <label for="img" class="form-label">Immagine*</label>
+                                <input type="file" class="form-control @error('img') is-invalid @enderror" id="img"
+                                    name="img" value="{{ old('img') ?? '' }}" required>
+                                @error('img')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
                 </div>
                 <div class="mb-3">
-                    <label for="img" class="form-label">Immagine:</label>
-                    <input type="text" class="form-control @error('img') is-invalid @enderror" id="img" name="img"
-                        value="{{ old('img') ?? '' }}">
-                </div>
-                <div class="mb-3">
-                    <label for="type" class="form-label">Tipi:</label>
-                    <select multiple name="types[]" id="" class="form-select">
-                        <option value="">Non ci sono tipi</option>
+                    <label class="mb-2">types*</label>
+                    <div class="checkbox-groups">
                         @foreach ($types as $type)
-                            <option value="{{ $type->id }}">{{ $type->name }}</option>
+                            <div class="form-check form-check-inline w-25">
+                                <input class="form-check-input @error('types') is-invalid @enderror" type="checkbox"
+                                    name="types[]" id="type{{ $type->id }}" value="{{ $type->id }}"
+                                    {{ in_array($type->id, old('types', [])) ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="type{{ $type->id }}">
+                                    {{ $type->name }}
+                                </label>
+                            </div>
                         @endforeach
-                    </select>
-                </div>
-                <textarea rows="6" class="form-control @error('description') is-invalid @enderror" id="description"
-                    name="description" placeholder="Inserisci una descrizione">{{ old('description') ?? '' }}</textarea>
-                @error('description')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-        </div>
-        <div class="mb-3 row">
-            <div class="col-12 col-md-6 mb-3">
-                <label for="address" class="form-label">Indirizzo*</label>
-                <input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address"
-                    value="{{ old('address') ?? '' }}" placeholder="Via, civico, città, provincia, CAP" required>
-                @error('address')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="col-12 col-md-6 mb-3">
-                <label for="img" class="form-label">Immagine*</label>
-                <input type="file" class="form-control @error('img') is-invalid @enderror" id="img" name="img"
-                    value="{{ old('img') ?? '' }}" required>
-                @error('img')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-        </div>
-        </div>
-        <div class="mb-3">
-            <label class="mb-2">types*</label>
-            <div class="checkbox-groups">
-                @foreach ($types as $type)
-                    <div class="form-check form-check-inline w-25">
-                        <input class="form-check-input @error('types') is-invalid @enderror" type="checkbox" name="types[]"
-                            id="type{{ $type->id }}" value="{{ $type->id }}"
-                            {{ in_array($type->id, old('types', [])) ? 'checked' : '' }}>
-
-                        <label class="form-check-label" for="type{{ $type->id }}">
-                            {{ $type->name }}
-                        </label>
                     </div>
-                @endforeach
-            </div>
-        </div>
+                </div>
 
-        <button type="submit" class="btn btn-primary">Aggiungi</button>
-        </form>
-        <a href="{{ route('admin.restaurants.index') }}" class="btn btn-warning">Torna alla lista dei ristoranti</a>
-        @endif
-    @else
-        <p>Questa pagina è disponibile solo per utenti autenticati. Effettua il login per accedere.</p>
-    @endauth
+                <button type="submit" class="btn btn-primary">Aggiungi</button>
+                </form>
+                <a href="{{ route('admin.restaurants.index') }}" class="btn btn-warning">Torna alla lista dei ristoranti</a>
+            @endif
+        @else
+            <p>Questa pagina è disponibile solo per utenti autenticati. Effettua il login per accedere.</p>
+        @endauth
     </div>
     </div>
 @endsection
