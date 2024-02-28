744e4096f9e09ec66372ddcaad662cbbe1e7b370
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-28 11:41:07 +0100

    Implement restaurant delete confermation

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -25,20 +25,56 @@
                             @endif
                         </div>
                         <div class="d-flex justify-content-center">
+                            <a href="{{ route('admin.dishes.index') }}"
+                                class="btn btn-primary m-2 w-100 d-flex justify-content-center align-items-center"><i
+                                    class="fa-solid fa-utensils me-3 fs-5"></i><span> Mostra menù</span></a>
                             <a href="{{ route('admin.restaurants.edit', $user->restaurant->id) }}"
-                                class="btn btn-info mx-2 mb-2">Edit</a>
-                            <form action="{{ route('admin.restaurants.destroy', $user->restaurant->id) }}" method="POST"
-                                class="d-inline-block mx-2 mb-2">
-                                @csrf
-                                @method('DELETE')
-                                <input type="submit" value="Delete" class="btn btn-danger">
-                            </form>
+                                class="btn btn-warning m-2 w-25 d-flex justify-content-center align-items-center"><i
+                                    class="fa-solid fa-pencil fs-5"></i></a>
+                            <!-- Button trigger modal -->
+                            <button type="button"
+                                class="btn btn-danger m-2 w-25 d-flex justify-content-center align-items-center"
+                                data-bs-toggle="modal" data-bs-target="#{{ $restaurant->id }}">
+                                <i class="fa-solid fa-trash-can fs-5"></i>
+                            </button>
+
                         </div>
+
                     </div>
+                    <form action="{{ route('admin.restaurants.destroy', $restaurant) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="{{ $restaurant->id }}" tabindex="-1"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content text-dark">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="warningTitle">ATTENZIONE</h5>
+                                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Sei sicuro di voler eliminare il ristorante {{ $restaurant->name }}?</p>
+                                        <p>Questa azione è irreversibile.</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary fw-bold"
+                                            data-bs-dismiss="modal">ANNULLA</button>
+                                        <input id="deleteBtn" class="btn btn-danger fw-bold" type="submit" value="ELIMINA">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            @else
-                <p>Nessun ristorante associato a questo utente.</p>
-            @endif
         </div>
+    </div>
+@else
+    <p>Nessun ristorante associato a questo utente.</p>
+    @endif
+    </div>
     </div>
 @endsection
