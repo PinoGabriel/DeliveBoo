3926427d8dca48cc28e1cee58eaf9ed665b33094
PinoGabriel <pinogabriel01@gmail.com>
2024-03-01 18:01:47 +0100

    Removed bottom visibility at conditions

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,8 +1,16 @@
 @php
     $user = auth()->user();
     $hasRestaurant = $user->restaurant !== null;
-@endphp
+    if ($hasRestaurant) {
+        $restaurantId = Illuminate\Support\Facades\Auth::user()->restaurant->id;
 
+        $orders = App\Models\Order::with(['restaurant', 'dishes'])
+            ->whereHas('restaurant', function ($query) use ($restaurantId) {
+                $query->where('id', $restaurantId);
+            })
+            ->get();
+    }
+@endphp
 
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
@@ -44,12 +52,25 @@
                                 </a>
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.restaurants.show' ? 'bg-secondary' : '' }}"
-                                    href="{{ $user->restaurant ? route('admin.restaurants.show', $user->restaurant) : route('admin.restaurants.create') }}">
-                                    <i class="fa-solid fas fa-building fa-lg fa-fw"></i> Ristorante
-                                </a>
-                            </li>
+                            @if ($hasRestaurant)
+                                <!-- Mostra il pulsante solo se c'è un ristorante -->
+                                <li class="nav-item">
+                                    <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.restaurants.show' ? 'bg-secondary' : '' }}"
+                                        href="{{ route('admin.restaurants.show', $user->restaurant) }}">
+                                        <i class="fa-solid fas fa-building fa-lg fa-fw"></i> Ristorante
+                                    </a>
+                                </li>
+
+                                <!-- Mostra il pulsante solo se c'è almeno un ordine per il ristorante -->
+                                @if (!$orders->isEmpty())
+                                    <li class="nav-item">
+                                        <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.orders.index' ? 'bg-secondary' : '' }}"
+                                            href="{{ route('admin.orders.index') }}">
+                                            <i class="fa-solid fa-clipboard-list fa-lg fa-fw"></i> Lista degli ordini
+                                        </a>
+                                    </li>
+                                @endif
+                            @endif
 
                             @if (!$hasRestaurant)
                                 <li class="nav-item">
@@ -68,13 +89,6 @@
                                     </a>
                                 </li>
                             @endif
-
-                            <li class="nav-item">
-                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.orders.index' ? 'bg-secondary' : '' }}"
-                                    href="{{ route('admin.orders.index') }}">
-                                    <i class="fa-solid fa-clipboard-list fa-lg fa-fw"></i> Lista degli ordini
-                                </a>
-                            </li>
 
                             <li class="nav-item">
                                 <a class="nav-link text-white" href="{{ route('logout') }}"
