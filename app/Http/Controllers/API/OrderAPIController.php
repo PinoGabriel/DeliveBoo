2df0aae1ad9f6e98fac14d846a61bbeecd2b2f18
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-06 23:32:53 +0100

    Create order post api controller

--- app/Http/Controllers/API/OrderAPIController.php
+++ app/Http/Controllers/API/OrderAPIController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+
 
 class OrderAPIController extends Controller
 {
@@ -32,6 +34,32 @@ class OrderAPIController extends Controller
         return response()->json([
             'success' => true,
             'payload' => Order::with(['restaurant', 'dishes'])->find($id)
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $order = new Order();
+        $order->client_name = $request->client_name;
+        $order->client_surname = $request->client_surname;
+        $order->client_mail = $request->client_mail;
+        $order->client_phone = $request->client_phone;
+        $order->restaurant_id = $request->restaurant_id;
+        $order->client_address = $request->client_address;
+        $order->total = $request->total;
+        $order->status = $request->status;
+        $order->save();
+
+        foreach ($request->dishes as $dish) {
+            DB::table('dish_order')->insert([
+                'dish_id' => $dish['id'],
+                'order_id' => $order->id,
+                'quantity' => $dish['quantity'],
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'payload' => $request->dishes[0]['quantity']
         ]);
     }
 }
