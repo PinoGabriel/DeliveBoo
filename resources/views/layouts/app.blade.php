a2f3ccf209d5a03326904f20a19a1734546391b2
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-11 00:19:16 +0100

    Change icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="icon" href="{{ url('/img/logo.svg') }}">
 
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
